4c12b4f4bd43bb78ebce686a724b73cffc404885
Halie Chmura <halie.chmura@gmail.com>
2021-08-21 15:33:06 -0400

    adding beginning of project

---
+++ silver-tongue/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
+
